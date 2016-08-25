f43d6a4386045c3aa05a51ec6989af9e038545d1
Dax <limeside@gmail.com>
2016-08-25 10:15:45 -0500

    Add base weapon class and sword class. :rabbit:

---
+++ src/Equipment/Weapon.java
@@ -0,0 +1,12 @@
+package Equipment;
+
+/*
+  Defines default weapon attributes.
+  - Dax
+ */
+public class Weapon {
+  String weaponName;
+  String weaponDesc;
+  int weaponDamage;
+  int weaponSpeed;
+}
